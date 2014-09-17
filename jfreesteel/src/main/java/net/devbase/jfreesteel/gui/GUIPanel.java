de6a37f119db827ec624f1f05dc6f4dd75642b4c
Goran Rakic <grakic@devbase.net>
2014-09-17 17:39:45 +0200

    Extend panel size

--- jfreesteel/src/main/java/net/devbase/jfreesteel/gui/GUIPanel.java
+++ jfreesteel/src/main/java/net/devbase/jfreesteel/gui/GUIPanel.java
@@ -116,8 +116,8 @@ public class GUIPanel extends JPanel {
     }
 
     public GUIPanel() {
-        setSize(new Dimension(700, 320));  // without layout manager
-        setPreferredSize(new Dimension(700, 320));  // with layout manager
+        setSize(new Dimension(730, 320));  // without layout manager
+        setPreferredSize(new Dimension(730, 320));  // with layout manager
         setBorder(new EmptyBorder(12, 12, 12, 12));
         setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 
